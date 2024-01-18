5209e1a00e41f5f1c3418e943c0d58cf03835de0
Cecilia <ccchimento@gmail.com>
2024-01-18 15:01:18 -0700

    update app.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,20 +4,11 @@ import "../stylesheets/App.css";
 import WestworldMap from "./WestworldMap";
 import HeadQuarters from "./Headquarters";
 
-// const initialState = {
-//   "high_plains": 0,
-//   "lowlands": 0,
-//   "under_construction": 0,
-//   "pariah": 0,
-//   "python_pass": 0,
-//   "badlands": 0
-// }
 
 function App() {
   const [allHosts, setAllHosts] = useState([]);
   const [areas, setAreas] = useState([]);
   const [selectedHost, setSelectedHost] = useState("");
-  //const [currentNumHosts, setCurrentNumHosts]=useState(initialState)
 
   const activeHosts = allHosts.filter((host) => host.active === true);
   const decomHosts = allHosts.filter((host) => host.active === false);
